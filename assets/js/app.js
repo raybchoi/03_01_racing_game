48e4bf37cf4fb010becaf946f997ad7b2fa2772f
raybchoi <raybchoi@gmail.com>
2017-08-23 14:39:20 -0700

    win counter and player speeds

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,55 +7,83 @@ const players = [
       'player': 1,
       'name': 'don',
       'img': 'assets/img/don_1.png',
+      'speed': '+=10%',
     },
     {
       'player': 2,
       'name': 'leo',
       'img': 'assets/img/leo_1.png',
+      'speed': '+=15%',
     },
     {
       'player': 3,
       'name': 'michael',
       'img': 'assets/img/michael_1.png',
+      'speed': '+=5%',
     },
     {
       'player': 4,
       'name': 'ralph',
       'img': 'assets/img/ralph_1.png',
+      'speed': '+=8%',
     },
   ];
+let playerOneWinCounter = 0;
+console.log(playerOneWinCounter);
+let playerTwoWinCounter = 0;
+console.log(playerTwoWinCounter);
 $(document).ready(function(){
 
   // DOM is fully loaded (this is jQuery)
-  // const selectPlayer =
+
+  const winCounter = function () {
+    $('.middleleft').html(`Player One Wins: ${playerOneWinCounter}`);
+    $('.middleright').html(`Player Two Wins: ${playerTwoWinCounter}`);
+  }
+  winCounter();
+  const restart = function() {
+    console.log('clicked')
+    $('#p1, #p2').remove();
+    selectPlayer();
+    gameOver = false;
+  }
+
   const selectPlayer = function () {
     console.log('selectPlayer function')
-    $('#p1').html(`<img src=${players[0].img}>`);
-    $('#p2').html(`<img src=${players[1].img}>`);
+    $('#start1').html(`<img id='p1' src=${players[0].img}>`);
+    $('#start2').html(`<img id='p2' src=${players[1].img}>`);
+    // $('#start1').html(`<p id=p1><img src=${players[0].img}></p>`)
+    // $('#start2').html(`<p id=p2><img src=${players[1].img}></p>`)
   }
+
   selectPlayer();
 
+  let startPlayerOnePosition = $('#p1').offset().left;
+  console.log('P1 Starting Postion: ', startPlayerOnePosition);
+  let startPlayerTwoPosition = $('#p2').offset().left;
+  console.log('P2 Starting Postion: ', startPlayerOnePosition)
 
   const movePlayer = function(event) {
-    // one space bar #p1 moves to the right when pushing '
+    // '39' => keypress => ' OR keyup => 222
     if ( gameOver === false ) {
-      if ( event.which === 39 ) {
-        $('#p1').animate({
-          'margin-left': '+=10%'
-        });
+      if ( event.which === 222 ) {
+        $('#p1').finish().animate({
+          'margin-left': `${players[0].speed}`
+        });;
         winner();
-        // a = 97
-      } else if ( event.which === 97 ) {
-        $('#p2').animate({
-          'margin-left': '+=10%'
-        });
+        // a = 65 => which for keydown /keyup or 97 for keypress //
+        // .finish then makes all animation stop so it does goes off screen
+      } else if ( event.which === 65 ) {
+        $('#p2').finish().animate({
+          'margin-left': `${players[1].speed}`
+        });;
         winner();
       }
     }
   }
 
   const winner = function() {
-    let winScreenSize = $(window).width()- ($(window).width()*.2)
+    let winScreenSize = $(window).width() - ($(window).width()*.2)
     let currentPlayerOnePosition = $('#p1').offset().left;
     let currentPlayerTwoPosition = $('#p2').offset().left;
     console.log('winner function is running');
@@ -68,14 +96,20 @@ $(document).ready(function(){
     console.log('Winning screen size: ', winScreenSize)
     if ( currentPlayerOnePosition >= winScreenSize ) {
       gameOver = true;
+      playerOneWinCounter++;
+      winCounter();
       console.log('Player 1 is the winner');
     } else if ( currentPlayerTwoPosition >= winScreenSize ) {
       gameOver = true;
+      playerTwoWinCounter++;
+      winCounter();
       console.log('Player 2 is the winner');
     }
   }
 
-  $(window).on('keydown', movePlayer);
+  $(window).on('keyup', movePlayer);
+
+  $('.reset').on('click', restart)
 
 
 });
@@ -95,5 +129,5 @@ $(window).on("keypress", function moveRight(event) {
 
 monitor events:
 monitorEvents(window, ["click", "keypress", "resize", "scroll"]);
-
+monitorEvents(window, ["keydown", "keypress"]);
 */
